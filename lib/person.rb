966068e56639ce6b905e09a194bf619522ee9ad4
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-02 03:01:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes:)
-    attributes.each {|key,value| self.send(("#{key}"), value)}
-  end
+  # def initialize(attributes:)
+  #   attributes.each {|key,value| self.send(("#{key}"), value)}
+  # end
 
 end
